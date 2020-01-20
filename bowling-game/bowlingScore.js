ad46d60661ed2da9d3da6dbed90ea2928aebbcf8
Varsha C L <clvarsha1998@gmail.com>
2020-01-20 12:31:57 +0530

    refractored code for roll function

--- bowling-game/bowlingScore.js
+++ bowling-game/bowlingScore.js
@@ -1,9 +1,9 @@
 let rollArray = [];
 const bowling = () => {
     let bowlingScore = 0;
-
-    let rollIndex = 0;
-    for (let frame = 0; frame < 10; frame++) {
+    let frame = 1;
+    let rollIndex;
+    for (rollIndex = 0; rollIndex < rollArray.length && frame <= 9;) {
 
         if (rollArray[rollIndex] < 10) //no strike case
         {
@@ -11,22 +11,48 @@ const bowling = () => {
             {
 
                 bowlingScore += frameScore(rollArray, rollIndex, 'spare');
+                console.log(rollIndex, bowlingScore, frame);
                 rollIndex += 2;
             }
             else {  //open frame case
 
                 bowlingScore += frameScore(rollArray, rollIndex, 'openFrame');
+                console.log(rollIndex, bowlingScore);
                 rollIndex += 2;
             }
         }
         else {
             //handling strike case
             bowlingScore += frameScore(rollArray, rollIndex, 'strike');
+            console.log(rollIndex, bowlingScore);
             rollIndex += 1;
         }
+        frame++;
 
+    }
+    if (frame == 10) {
+        if (rollArray[rollIndex] < 10) //no strike case
+        {
+            if ((rollArray[rollIndex] + rollArray[rollIndex + 1]) == 10) //spare case
+            {
 
+                bowlingScore += frameScore(rollArray, rollIndex, 'spare');
+                console.log(rollIndex, bowlingScore);
+                rollIndex += 2;
+            }
+            else {  //open frame case
 
+                bowlingScore += frameScore(rollArray, rollIndex, 'openFrame');
+                console.log(rollIndex, bowlingScore);
+                rollIndex += 2;
+            }
+        }
+        else {
+            //handling strike case
+            bowlingScore += frameScore(rollArray, rollIndex, 'strike');
+            console.log(rollIndex, bowlingScore);
+            rollIndex += 1;
+        }
     }
     rollArray = [];
     return bowlingScore;
